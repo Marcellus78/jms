6f1044d64c7dbb6679f761a8d0b1385276169fc9
marcellus <War13win23@>
2020-09-14 21:13:09 +0200

    adding configuration, scheduler, converter and simple message

---
+++ src/main/java/com/marcllus/jms/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package com.marcllus.jms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = -8166403478187294105L;
+
+    private UUID id;
+    private String message;
+}
